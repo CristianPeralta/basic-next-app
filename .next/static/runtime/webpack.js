aa47b3e855c4c67055fe7fc6baaa8b0a36e59191
CristianPeralta <cbperaltasegura@gmail.com>
2020-03-20 13:15:51 -0500

    add user component

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "bab1d69a97e2d4ef3c30";
+/******/ 	var hotCurrentHash = "d8514425542b94f72182";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ components/User.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const user = (props) => {
+    return (
+        <dib>
+
+        </dib>
+    );
+};
+
+export default user;
