07f02c5dcd11343bac72a177188b3e252f43affb
CristianPeralta <cbperaltasegura@gmail.com>
2020-03-20 13:27:52 -0500

    add error component

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "22ec2df711cf182e8d6e";
+/******/ 	var hotCurrentHash = "4bab735bd866841bf1a7";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
